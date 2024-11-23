3374d8c777eb25f253af459e6aa78221d079164a
ThreadedbyP <id1155562@email.ccbcmd.edu>
2024-11-22 19:57:18 -0500

    Add files via upload

--- Lab10/script.js
+++ Lab10/script.js
@@ -1,10 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sidebar = document.getElementById("sidebar");
-    const menuBtn = document.getElementById("menuBtn");
-  
-    menuBtn.addEventListener("click", () => {
-      // Toggle sidebar visibility
-      sidebar.classList.toggle("sidebar-active");
-    });
-  });
-  
+// JavaScript for toggling the side navigation menu
+function toggleMenu() {
+  const sideNav = document.getElementById('side-nav');
+  sideNav.classList.toggle('open');
+}

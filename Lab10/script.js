9f6eeefb74f5f3c5afb3ca8d37c2aebfb53333dc
ThreadedbyP <id1155562@email.ccbcmd.edu>
2024-11-23 14:57:59 -0500

    Delete Lab10 directory

--- Lab10/script.js
+++
@@ -1,8 +0,0 @@
-function toggleNav() {
-    var sidenav = document.getElementById("mySidenav");
-    if (sidenav.style.width === "250px") {
-        sidenav.style.width = "0";
-    } else {
-        sidenav.style.width = "250px";
-    }
-}

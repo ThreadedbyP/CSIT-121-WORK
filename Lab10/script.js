1ac286de27bbeded109b5c43497c26f42cbb87b1
ThreadedbyP <id1155562@email.ccbcmd.edu>
2024-11-23 20:08:00 -0500

    Add files via upload

--- Lab10/script.js
+++ Lab10/script.js
@@ -1,13 +1,7 @@
-// Open the overlay menu
 function openNav() {
-    const overlay = document.getElementById("myNav");
-    overlay.style.width = "100%"; // Expand overlay
-    document.body.style.overflow = "hidden"; // Prevent scrolling
+    document.getElementById("myNav").style.width = "100%";
 }
 
-// Close the overlay menu
 function closeNav() {
-    const overlay = document.getElementById("myNav");
-    overlay.style.width = "0"; // Collapse overlay
-    document.body.style.overflow = "auto"; // Allow scrolling
+    document.getElementById("myNav").style.width = "0%";
 }
